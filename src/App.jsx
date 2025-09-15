6c4f7aa93a8dc54ee092fbfcef495d808f76b1d9
Memona <memonaasgharasgharali@gmail.com>
2025-09-15 21:49:45 +0500

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react';
+// import { useState } from 'react'
 import Navbar from './components/Navbar';
+import React, { useState, useEffect } from 'react';
 import { FaEdit } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
 import { v4 as uuidv4 } from 'uuid';
