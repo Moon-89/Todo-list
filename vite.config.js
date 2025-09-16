afb030dbed9a31c8df67b9053e9ab661a1c13fe1
Memona <memonaasgharasgharali@gmail.com>
2025-09-16 23:09:34 +0500

    build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/Todo-list",
+  build: {
+    outDir: 'dist' // or leave default if not customizing
+  }
 })
